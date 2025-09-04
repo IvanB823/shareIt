b875de8b1d61b92732da174b7dd0f09222544dc3
Ivan Baturo <baturo.ivan@bk.ru>
2025-09-04 15:16:01 +0300

    feat: Dockerfile and pom.xml for server added

--- server/src/test/java/ru/practicum/shareit/ShareItTests.java
+++ server/src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,9 +2,7 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class ShareItTests {
 
